11859db37e8294b4a20f6c7641b93791e359395b
liupandeng <liupandeng@bicai365.com>
2020-03-27 16:30:37 +0800

    build

---
+++ build/precache-manifest.3344029ba1ee76ac6ebc3eba51fdbca8.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382ea6635a789cb9b82da786bd79c777",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9bbabf5a8257384bb91e",
+    "url": "./static/css/0.5cb72882.chunk.css"
+  },
+  {
+    "revision": "394b1d0ae521d88a9cb8",
+    "url": "./static/css/5.51f6e1db.chunk.css"
+  },
+  {
+    "revision": "871b5decb66a8f4f918d",
+    "url": "./static/css/6.585cdb6c.chunk.css"
+  },
+  {
+    "revision": "4bb57ff46635505e9429",
+    "url": "./static/css/8.636066e4.chunk.css"
+  },
+  {
+    "revision": "520b91a06b42ccb421ef",
+    "url": "./static/css/main.fb94cbca.chunk.css"
+  },
+  {
+    "revision": "9bbabf5a8257384bb91e",
+    "url": "./static/js/0.f08e9a90.chunk.js"
+  },
+  {
+    "revision": "29d7d06803c420ec989f",
+    "url": "./static/js/1.ebb278c6.chunk.js"
+  },
+  {
+    "revision": "ac2ec41b05d3575b8f71",
+    "url": "./static/js/10.a0b63555.chunk.js"
+  },
+  {
+    "revision": "a0c2e3a0c5025d374864",
+    "url": "./static/js/11.9dd5fc90.chunk.js"
+  },
+  {
+    "revision": "46f962cb11cf06b53ef4",
+    "url": "./static/js/2.6dc16097.chunk.js"
+  },
+  {
+    "revision": "efb458a97117f132bd28b58fc490db1f",
+    "url": "./static/js/2.6dc16097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394b1d0ae521d88a9cb8",
+    "url": "./static/js/5.54d107d6.chunk.js"
+  },
+  {
+    "revision": "e9eb83b6a2290cb6aaaf889dc89cb319",
+    "url": "./static/js/5.54d107d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871b5decb66a8f4f918d",
+    "url": "./static/js/6.758c58af.chunk.js"
+  },
+  {
+    "revision": "52bb731d1eb6b3cf84a7",
+    "url": "./static/js/7.f5e14215.chunk.js"
+  },
+  {
+    "revision": "4bb57ff46635505e9429",
+    "url": "./static/js/8.9b3dac2f.chunk.js"
+  },
+  {
+    "revision": "33f176fdd9791c0fd5c6",
+    "url": "./static/js/9.ab4ecd91.chunk.js"
+  },
+  {
+    "revision": "520b91a06b42ccb421ef",
+    "url": "./static/js/main.8718785e.chunk.js"
+  },
+  {
+    "revision": "6d1b08f640e82aa910f7",
+    "url": "./static/js/runtime-main.ab022605.js"
+  },
+  {
+    "revision": "8e74427a72de5f2fb609710a013b2723",
+    "url": "./static/media/PayResources.8e74427a.jpg"
+  },
+  {
+    "revision": "4ea9a7b350c52d968aa762fd24590323",
+    "url": "./static/media/articlesDaily.4ea9a7b3.jpg"
+  },
+  {
+    "revision": "6f7f3ecd80136e71444a4946adc441e8",
+    "url": "./static/media/avator.6f7f3ecd.jpg"
+  },
+  {
+    "revision": "5c899bf779380c45632732acd6f9f33d",
+    "url": "./static/media/bg.5c899bf7.jpg"
+  },
+  {
+    "revision": "dcefcf9f7ce3cf4842c9bb23d5f16644",
+    "url": "./static/media/cart.dcefcf9f.jpg"
+  },
+  {
+    "revision": "ed2e23ea2bd881015d02f78ae7d737d1",
+    "url": "./static/media/category.ed2e23ea.jpg"
+  },
+  {
+    "revision": "7e67b2c0ee537a1a4c0ea7e1595016cf",
+    "url": "./static/media/clothes.7e67b2c0.jpg"
+  },
+  {
+    "revision": "1b7cfc92089372512dffd4a16cae4f6e",
+    "url": "./static/media/collectGoods.1b7cfc92.jpg"
+  },
+  {
+    "revision": "2a89c47afc21f37ae4803662637bba90",
+    "url": "./static/media/generationEvaluation.2a89c47a.jpg"
+  },
+  {
+    "revision": "ddccf5d70c9731f2c5298d05d4d8bb12",
+    "url": "./static/media/home.ddccf5d7.jpg"
+  },
+  {
+    "revision": "5c25e1cb9735adf1b556f42b8df9b7d1",
+    "url": "./static/media/iconfont.5c25e1cb.svg"
+  },
+  {
+    "revision": "7e008a771d5079e5051b632adcff1243",
+    "url": "./static/media/iconfont.7e008a77.eot"
+  },
+  {
+    "revision": "d828102abc66b0a0a7cd5eb3f9b4e2b4",
+    "url": "./static/media/iconfont.d828102a.ttf"
+  },
+  {
+    "revision": "ab1dded153995021ee2ae7dcf062dbb1",
+    "url": "./static/media/info.ab1dded1.jpg"
+  },
+  {
+    "revision": "fe42f6654a59d3741578898fec9a46c1",
+    "url": "./static/media/my.fe42f665.jpg"
+  },
+  {
+    "revision": "38c8a5991de8136aa6c1200ad0c38d36",
+    "url": "./static/media/payChannel.38c8a599.jpg"
+  },
+  {
+    "revision": "4c61a8157d4053467f8d4acc720bee25",
+    "url": "./static/media/points.4c61a815.jpg"
+  },
+  {
+    "revision": "5cb116a9b3624749fc2fe4d01c678997",
+    "url": "./static/media/recommendation.5cb116a9.jpg"
+  },
+  {
+    "revision": "49fed055e1de3b22bf739455f27e631a",
+    "url": "./static/media/refund.49fed055.jpg"
+  },
+  {
+    "revision": "22bf20ab30aa19a2954109c18058c15d",
+    "url": "./static/media/residence.22bf20ab.jpg"
+  },
+  {
+    "revision": "2904355f60745d46ac68964e32140bf4",
+    "url": "./static/media/shoes.2904355f.jpg"
+  },
+  {
+    "revision": "365503530804d029d0b543153915b358",
+    "url": "./static/media/substitutePay.36550353.jpg"
+  },
+  {
+    "revision": "4270a53edf1e6fbc2a4f1b9e86df8835",
+    "url": "./static/media/swiper1.4270a53e.jpg"
+  },
+  {
+    "revision": "558f66c9ffa33524435c53532b6cf943",
+    "url": "./static/media/swiper2.558f66c9.jpg"
+  },
+  {
+    "revision": "16c942a0c199d76343456a96254b71d6",
+    "url": "./static/media/swiper3.16c942a0.jpg"
+  },
+  {
+    "revision": "8200722f147babe97d1a62569c05e8c1",
+    "url": "./static/media/swiper4.8200722f.jpg"
+  },
+  {
+    "revision": "25b834e901855ed9a557c2171dce718b",
+    "url": "./static/media/wallet.25b834e9.jpg"
+  }
+]);
